a39a661372765c5f0d2dee20b17b9849e4186860
thanh <highofearths123@gmail.com>
2025-03-27 15:17:02 +0700

    1.0.2

--- TravelApp/urls.py
+++ TravelApp/urls.py
@@ -21,8 +21,8 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('mapapp.urls')),
-    path('register/', include('mapapp.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='mapapp/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
+    # path('register/', include('mapapp.urls')),
+    # path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
+    # path('logout/', auth_views.LogoutView.as_view(next_page='/'), name='logout'),
     path('accounts/', include('allauth.urls')),
 ]
